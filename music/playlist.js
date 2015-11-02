a3e83c5d693dda307b8628a03dff1131b4eacd4f
jiejiecat <jiejiecat@outlook.com>
2015-11-03 01:04:19 +0800

    update BGM

--- music/playlist.js
+++ music/playlist.js
@@ -1,8 +1,17 @@
+//var playlist = [
+//{"name": "dying in the sun.mp3"},
+//{"name": "never grow old.mp3"},
+//{"name": "you and me.mp3"},
+//{"name": "can't take my eyes off you.mp3"},
+//{"name": "rattle my bones.mp3"},
+//{"name": "true color.mp3"},
+//]
 var playlist = [
-{"name": "dying in the sun.mp3"},
-{"name": "never grow old.mp3"},
-{"name": "you and me.mp3"},
 {"name": "can't take my eyes off you.mp3"},
-{"name": "rattle my bones.mp3"},
-{"name": "true color.mp3"},
+{"name": "dying in the sun.mp3"},
+{"name": "new soul.mp3"},
+{"name": "silent all these years.mp3"},
+{"name": "克卜勒.mp3"},
+{"name": "失踪.mp3"},
+{"name": "昨夜小楼又东风.mp3"},
 ]
